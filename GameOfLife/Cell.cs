7f2ec6f972418590a859b8c5bb3738c95a612b9b
mikgr <m.gautier.roesen@gmail.com>
2018-07-03 19:38:44 +0200

    move cell to own file

---
+++ GameOfLife/Cell.cs
@@ -0,0 +1,24 @@
+﻿namespace GameOfLifeProject.Cs
+{
+    public class Cell
+    {
+        public Cell(CellContent cellContent, int top, int left)
+        {
+            CellContent = cellContent;
+            Top = top;
+            Left = left;
+        }
+
+        public void MoveNext()
+        {
+            CellContent = CellContent.NextCellContent;
+        }
+
+        public CellContent CellContent { get; private set; }
+
+        public int Top { get; protected set; }
+
+        public int Left { get; protected set; }
+
+    }
+}
